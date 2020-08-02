0a9b733206b3fe7b5fd81a7b9e7f522a5bff5f48
WasAlexHere <sethtorston@mail.ru>
2020-08-02 21:02:50 +0300

    added a few file for PageObject pattern

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,21 @@
+import time
+'''
+link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    #time.sleep(10)
+    go_to_login_page(browser)
+
+def test_add_to_cart(browser):
+    page = ProductPage(link, browser)   # инициализируем объект Page Object
+    page.open()                           # открываем страницу в браузере
+    page.should_be_add_to_cart_button()   # проверяем что есть кнопка добавления в корзину
+    page.add_product_to_cart()            # жмем кнопку добавить в корзину
+    page.should_be_success_message()      # проверяем что есть сообщение с нужным текстом
+'''
+
