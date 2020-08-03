349998320899c29db9c666953ce5a7d0cbe98c3c
WasAlexHere <sethtorston@mail.ru>
2020-08-03 20:42:50 +0300

    updated test_product_page.py due to incorrect verification

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,3 +7,13 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ItemPagesLocators.ADD_TO_BASKET_BUTTON), "Add to basket button is not presented"
         self.add_to_basket_button = self.browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button")
         self.add_to_basket_button.click()
+
+    def item_should_be_added_to_basket(self):
+        self.item = self.browser.find_element_by_xpath("//*[@id='content_inner']/article/div[1]/div[2]/h1")
+        self.item_text = self.item.text
+        print("%a was added to basket!" % self.item_text)
+
+    def count_price_of_basket(self):
+        self.price_of_the_basket = self.browser.find_element_by_xpath("//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
+        self.price_of_the_basket_text = self.price_of_the_basket.text
+        print("The Current price of your basket is: ", self.price_of_the_basket_text)
--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
     page.solve_quiz_and_get_code()
+    page.item_should_be_added_to_basket()
+    page.count_price_of_basket()
