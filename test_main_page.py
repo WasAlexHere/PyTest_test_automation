b68c684412ae05dea5e0506402bb6edb664b60d7
WasAlexHere <sethtorston@mail.ru>
2020-08-03 17:10:34 +0300

    updated test_main_page.py file

--- test_main_page.py
+++ test_main_page.py
@@ -1,32 +1,35 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
+link = "http://selenium1py.pythonanywhere.com/"
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page.go_to_login_page()          # выполняется метод страницы - переход на новую страницу
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.should_be_login_link()      # выполняем метод страницы - переходим на страницу логина
 
 def test_guest_should_verify_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_login_url()       # выполняем метод страницы - проверка на корректный url
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()       # выполняем метод страницы - проверка на корректный url
 
 def test_guest_should_login(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_login_form()      # выполняем метод страницы - проверка на наличие логин формы
+    page.go_to_login_page()      # выполняем метод страницы - проверка на наличие логин формы
+    login_page = LoginPage(browser, browser.current_url)  # выполняется метод страницы - переход на новую страницу
+    login_page.should_be_login_form()
 
 def test_guest_should_be_registrated(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.should_be_register_form()   # выполняем метод страницы - проверка на наличие регистрационной формы
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()   # выполняем метод страницы - проверка на наличие регистрационной формы
